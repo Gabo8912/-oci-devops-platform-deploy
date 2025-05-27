e6cc9596484339802daeb7ef03bb5edaa021fd3c
Gabo8912 <128503197+Gabo8912@users.noreply.github.com>
2025-05-27 08:43:26 -0600

    Update variables.tf

--- devops_oke/variables.tf
+++ devops_oke/variables.tf
@@ -1,14 +1,13 @@
 variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
 variable "region" {}
 variable "compartment_id" {}
-
 
 variable "app_name" {
   default     = "DevOps"
   description = "Application name. Will be used as prefix to identify resources, such as OKE, VCN, DevOps, and others"
 }
 
-
+variable "release"{
+  description = "Reference Architecture Release (OCI Architecture Center)"
+  default     = "1.1"
+}
