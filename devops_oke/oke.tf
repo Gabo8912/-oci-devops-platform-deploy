51e4a3199fe1a12d4857260d6a0b99b48ceb7204
Gabo8912 <128503197+Gabo8912@users.noreply.github.com>
2025-05-27 19:31:02 -0600

    Update oke.tf

--- devops_oke/oke.tf
+++ devops_oke/oke.tf
@@ -1,12 +1,19 @@
-# Copyright (c) 2020, 2021 Oracle and/or its affiliates. All rights reserved.
-# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
-# 
+/*
+  Updated oke.tf to use an existing cluster via data source instead of creating new one.
+*/
 
+# Read existing OKE cluster
+data "oci_containerengine_cluster" "oke_cluster" {
+  cluster_id = var.existent_oke_cluster_id
+}
+
+# Optionally create new cluster if needed (count = 0 here, as create_new_oke_cluster=false)
 resource "oci_containerengine_cluster" "oke_cluster" {
-  compartment_id     = local.oke_compartment_id
-  kubernetes_version = (var.k8s_version == "Latest") ? local.cluster_k8s_latest_version : var.k8s_version
-  name               = "${var.app_name} (${random_string.deploy_id.result})"
-  vcn_id             = oci_core_virtual_network.oke_vcn[0].id
+  count               = var.create_new_oke_cluster ? 1 : 0
+  compartment_id      = local.oke_compartment_id
+  kubernetes_version  = (var.k8s_version == "Latest") ? local.cluster_k8s_latest_version : var.k8s_version
+  name                = "${var.app_name} (${random_string.deploy_id.result})"
+  vcn_id              = oci_core_virtual_network.oke_vcn[0].id
 
   endpoint_config {
     is_public_ip_enabled = (var.cluster_endpoint_visibility == "Private") ? false : true
@@ -16,7 +23,7 @@ resource "oci_containerengine_cluster" "oke_cluster" {
     service_lb_subnet_ids = [oci_core_subnet.oke_lb_subnet[0].id]
     add_ons {
       is_kubernetes_dashboard_enabled = var.cluster_options_add_ons_is_kubernetes_dashboard_enabled
-      is_tiller_enabled               = false # Default is false, left here for reference
+      is_tiller_enabled               = false
     }
     admission_controller_options {
       is_pod_security_policy_enabled = var.cluster_options_admission_controller_options_is_pod_security_policy_enabled
@@ -26,13 +33,13 @@ resource "oci_containerengine_cluster" "oke_cluster" {
       pods_cidr     = lookup(var.network_cidrs, "PODS-CIDR")
     }
   }
-  # kms_key_id = var.use_encryption_from_oci_vault ? (var.create_new_encryption_key ? oci_kms_key.mushop_key[0].id : var.existent_encryption_key_id) : null
 
   count = var.create_new_oke_cluster ? 1 : 0
 }
 
 resource "oci_containerengine_node_pool" "oke_node_pool" {
-  cluster_id         = oci_containerengine_cluster.oke_cluster[0].id
+  # Use the data source ID if not creating new
+  cluster_id         = var.create_new_oke_cluster ? oci_containerengine_cluster.oke_cluster[0].id : data.oci_containerengine_cluster.oke_cluster.id
   compartment_id     = local.oke_compartment_id
   kubernetes_version = (var.k8s_version == "Latest") ? local.node_pool_k8s_latest_version : var.k8s_version
   name               = var.node_pool_name
@@ -42,13 +49,11 @@ resource "oci_containerengine_node_pool" "oke_node_pool" {
   node_config_details {
     dynamic "placement_configs" {
       for_each = data.oci_identity_availability_domains.ADs.availability_domains
-
       content {
         availability_domain = placement_configs.value.name
         subnet_id           = oci_core_subnet.oke_nodes_subnet[0].id
       }
     }
-    #size = var.cluster_autoscaler_enabled ? var.cluster_autoscaler_min_nodes : var.num_pool_workers
     size = var.num_pool_workers
   }
 
@@ -72,40 +77,4 @@ resource "oci_containerengine_node_pool" "oke_node_pool" {
   }
 
   count = var.create_new_oke_cluster ? 1 : 0
-}
-
-resource "oci_identity_compartment" "oke_compartment" {
-  compartment_id = var.compartment_id
-  name           = "${local.app_name_normalized}-${random_string.deploy_id.result}"
-  description    = "${var.app_name} ${var.oke_compartment_description} (Deployment ${random_string.deploy_id.result})"
-  enable_delete  = true
-
-  count = var.create_new_compartment_for_oke ? 1 : 0
-}
-locals {
-  #oke_compartment_id = var.create_new_compartment_for_oke ? oci_identity_compartment.oke_compartment.0.id : var.compartment_id
-  oke_compartment_id = var.compartment_id
-}
-
-# Local kubeconfig for when using Terraform locally. Not used by Oracle Resource Manager
-resource "local_file" "kubeconfig" {
-  content  = data.oci_containerengine_cluster_kube_config.oke_cluster_kube_config.content
-  filename = "generated/kubeconfig"
-}
-
-# Generate ssh keys to access Worker Nodes, if generate_public_ssh_key=true, applies to the pool
-resource "tls_private_key" "oke_worker_node_ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
-# Get OKE options
-locals {
-  cluster_k8s_latest_version   = reverse(sort(data.oci_containerengine_cluster_option.oke.kubernetes_versions))[0]
-  node_pool_k8s_latest_version = reverse(sort(data.oci_containerengine_node_pool_option.oke.kubernetes_versions))[0]
-}
-
-# Checks if is using Flexible Compute Shapes
-locals {
-  is_flexible_node_shape = contains(local.compute_flexible_shapes, var.node_pool_shape)
 }
